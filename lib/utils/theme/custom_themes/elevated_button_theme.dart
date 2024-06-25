8d44aef3bd3a10fdd02a2a5ef9b6c119e4dbd48c
Prayas Dhand <prayasdhand13@gmail.com>
2024-06-25 16:18:55 +0530

    starting

---
+++ lib/utils/theme/custom_themes/elevated_button_theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TElevatedButtonTheme {
+  TElevatedButtonTheme._();
+
+  static final lightElevatedButtonTheme = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.blue,
+
+    )
+    ,);
+
+
+}
