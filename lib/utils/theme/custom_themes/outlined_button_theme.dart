3b9c71aad0820691b498d9b74540b4a97192eb74
Prayas Dhand <prayasdhand13@gmail.com>
2024-06-26 11:01:38 +0530

    starting

---
+++ lib/utils/theme/custom_themes/outlined_button_theme.dart
@@ -0,0 +1,8 @@
+import'package:flutter/material.dart';
+
+class TOutlinedButtonTheme{
+  TOutlinedButtonTheme._();
+
+  static OutlinedButtonThemeData lightOutlinedButtonTheme = OutlinedButtonThemeData();
+  static OutlinedButtonThemeData darkOutlinedButtonTheme = OutlinedButtonThemeData();
+}
---
+++ lib/utils/theme/custom_themes/text_field_theme.dart
@@ -0,0 +1,7 @@
+import'package:flutter/material.dart';
+
+class TTextFieldTheme{
+  TTextFieldTheme._();
+
+
+}
